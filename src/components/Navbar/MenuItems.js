50b42ef62598f082b2d65a62cd35abaf25117923
AakashPawarAP <aakashpawar97@gmail.com>
2021-11-22 18:57:21 +0530

    first commit

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,32 @@
+export const MenuItems = [
+  {
+    title: "Home",
+    url: "#",
+    cName: "nav-links",
+  },
+  {
+    title: "About",
+    url: "#",
+    cName: "nav-links",
+  },
+  {
+    title: "Portfolio",
+    url: "#",
+    cName: "nav-links",
+  },
+  {
+    title: "Resume",
+    url: "#",
+    cName: "nav-links",
+  },
+  {
+    title: "Contact",
+    url: "#",
+    cName: "nav-links",
+  },
+  {
+    title: "Sign up",
+    url: "#",
+    cName: "nav-links-mobile",
+  },
+];
